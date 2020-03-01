be394c42057505786679a7ba4dd607d97d9c759c
Seth Broughton <broughton.24@gmail.com>
2020-03-01 10:56:53 -0500

    basic functionality

--- broughton.flowchain/src/main/java/com/broughton/flowchain/Block.java
+++ broughton.flowchain/src/main/java/com/broughton/flowchain/Block.java
@@ -8,6 +8,7 @@ public class Block {
 	public String previousHash;
 	private String data;
 	private long timeStamp;
+	private int nonce;
 	
 	//Constructor
 	public Block(String data, String previousHash) {
@@ -19,9 +20,18 @@ public class Block {
 	
 	//Methods
 	public String calculateHash() {
-		String input = previousHash + Long.toString(timeStamp) + data;
+		String input = previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data;
 		String calculatedHash = StringUtil.applySha256(input);
 		return calculatedHash;
+	}
+	
+	public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
 	}
 
 }
--- broughton.flowchain/src/main/java/com/broughton/flowchain/flowchain.java
+++ broughton.flowchain/src/main/java/com/broughton/flowchain/flowchain.java
@@ -6,13 +6,25 @@ import com.google.gson.*;
 public class flowchain {
 
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int difficulty = 5;
 
 	public static void main(String[] args) {
 
 		blockchain.add(new Block("The first block", "0"));
+		System.out.println("Trying to mine block 1...");
+		blockchain.get(0).mineBlock(difficulty);
+		
+		
 		blockchain.add(new Block("The second block", blockchain.get(blockchain.size() - 1).hash));
+		System.out.println("Trying to mine block 2...");
+		blockchain.get(1).mineBlock(difficulty);
+		
 		blockchain.add(new Block("The third block", blockchain.get(blockchain.size() - 1).hash));
+		System.out.println("Trying to mine block 3...");
+		blockchain.get(2).mineBlock(difficulty);
 
+		System.out.println("\nBlockchain is Valid: " + isValid());
+		
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 		System.out.print(blockchainJson);
 	}
@@ -20,6 +32,8 @@ public class flowchain {
 	public static boolean isValid() {
 		Block previousBlock;
 		Block currentBlock;
+		
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
 		for (int i = 1; i < blockchain.size(); i++) {
 			currentBlock = blockchain.get(i);
@@ -30,6 +44,11 @@ public class flowchain {
 			}
 			if(!previousBlock.hash.equals(currentBlock.previousHash)) {
 				return false;	
+			}
+			
+			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined");
+				return false;
 			}
 		}
 		return true;
